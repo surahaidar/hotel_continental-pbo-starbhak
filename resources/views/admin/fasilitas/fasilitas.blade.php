57e9900660312a21aba9cb2cd082313758902565
raphaelimmanuel1711 <rph171104@gmail.com>
2022-07-29 20:37:52 +0700

    import

--- resources/views/admin/fasilitas/fasilitas.blade.php
+++ resources/views/admin/fasilitas/fasilitas.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <h1 class="text-center">Fasilitas Kamar</h1>
 
-<a href="/tambahkamar" class="btn btn-success mt-5 mb-4">tambah</a>
+<a href="/tambahfasilitas" class="btn btn-success mt-5 mb-4">tambah</a>
 <table class="table table-light table-striped">
     <thead>
         <tr>
@@ -23,12 +23,12 @@
             <td>{{ $row->kamar }}</td>
             <td>{{ $row->nama }}</td>
             <td style="display: flex;">
-                <form action="/deletedatakamar/{{$row->id}}" method="post" style="margin-right: 20px">
+                <form action="/deletedatafasilitas/{{$row->id}}" method="post" style="margin-right: 20px">
                   @csrf
                   <button type="submit" class="btn btn-danger">Hapus</button>
                   @method('delete')
                 </form>
-                <a type="button" href="/tampilankamar/{{$row->id}}" class="btn btn-warning">Edit</a>                                                         
+                <a type="button" href="/tampilanfasilitas/{{$row->id}}" class="btn btn-warning">Edit</a>                                                         
             </td>
         </tr>
         @endforeach
