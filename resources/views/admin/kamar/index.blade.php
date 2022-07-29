4838357db8178ce433df3d1d2cb4a6f8a2fb91a0
raphaelimmanuel1711 <rph171104@gmail.com>
2022-07-29 18:29:52 +0700

    first commit

---
+++ resources/views/admin/kamar/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.main')
+
+@section('content')
+<h1 class="text-center">Kamar</h1>
+
+<a href="/tambahkamar" class="btn btn-success mt-5 mb-4">tambah</a>
+<table class="table table-light table-striped">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">No Kamar</th>
+            <th scope="col">Harga</th>
+            <th scope="col">Kapasitas</th>
+            <th scope="col">Tipe Kamar</th>
+            <th scope="col">Action</th>
+        </tr>
+    </thead>
+    @php
+        $no=1
+    @endphp
+    <tbody>
+        @foreach ($data as $row)            
+        <tr>
+            <th scope="row">{{ $loop->iteration  }}</th>
+            <td>{{ $row->nokamar }}</td>
+            <td>{{ $row->harga }}</td>
+            <td>{{ $row->kapasitas }}</td>
+            <td>{{ $row->tipe }}</td>
+            <td style="display: flex;">
+                <form action="/deletedatakamar/{{$row->id}}" method="post" style="margin-right: 20px">
+                  @csrf
+                  <button type="submit" class="btn btn-danger">Hapus</button>
+                  @method('delete')
+                </form>
+                <a type="button" href="/tampilankamar/{{$row->id}}" class="btn btn-warning">Edit</a>                                                         
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,149 @@
+<!--
+
+=========================================================
+* Argon Dashboard - v1.1.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>
+        Argon Dashboard - Free Dashboard for Bootstrap 4 by Creative Tim
+    </title>
+    <!-- Favicon -->
+    <link href="{{ asset('/assets/img/brand/favicon.png') }}" rel="icon" type="image/png">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!-- Icons -->
+    <link href="{{ asset('/assets/js/plugins/nucleo/css/nucleo.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link href="{{ asset('/assets/css/argon-dashboard.css?v=1.1.2') }}" rel="stylesheet" />
+</head>
+
+<body class="">
+    <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
+        <div class="container-fluid">
+            <!-- Toggler -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main"
+                aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Brand -->
+            <a class="navbar-brand pt-0" href="./index.html">
+                <img src="{{ asset('/assets/img/brand/blue.png') }}" class="navbar-brand-img" alt="...">
+            </a>
+            <!-- Collapse -->
+            <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+                <!-- Collapse header -->
+                <div class="navbar-collapse-header d-md-none">
+                    <div class="row">
+                        <div class="col-6 collapse-brand">
+                            <a href="./index.html">
+                                <img src="./assets/img/brand/blue.png">
+                            </a>
+                        </div>
+                        <div class="col-6 collapse-close">
+                            <button type="button" class="navbar-toggler" data-toggle="collapse"
+                                data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false"
+                                aria-label="Toggle sidenav">
+                                <span></span>
+                                <span></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <!-- Form -->
+                <form class="mt-4 mb-3 d-md-none">
+                    <div class="input-group input-group-rounded input-group-merge">
+                        <input type="search" class="form-control form-control-rounded form-control-prepended"
+                            placeholder="Search" aria-label="Search">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <span class="fa fa-search"></span>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <!-- Navigation -->
+                
+                <ul class="navbar-nav">
+                    {{-- <h4 class="text-center">Haloo {{ auth()->user()->nama }} ! </h4> --}}
+                    <li class="nav-item">
+                        <a class="nav-link " href="/home">
+                            <i class="ni ni-single-02 text-yellow"></i> Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="/kamar">
+                            <i class="ni ni-single-02 text-yellow"></i> Kamar
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="/fasilitas">
+                            <i class="ni ni-bullet-list-67 text-red"></i> Fasilitas Kamar
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="/fasilitasumum">
+                            <i class="ni ni-bullet-list-67 text-red"></i> Fasilitas Umum
+                        </a>
+                    </li>
+                </ul>
+                <!-- Divider -->
+                <hr class="my-3">
+                <!-- Navigation -->
+                <ul class="navbar-nav mb-md-3">
+                    <li class="nav-item">
+                        <form action="logout" method="GET">
+                            @csrf
+                            <a class="nav-link"> 
+                                <button type="submit" class="btn btn-outline-dark"><i class="ni ni-user-run text-info"></i>  Logout</button>
+                            </a>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="main-content">
+        
+        <div class="container-fluid mt-7">
+            @yield('content')
+            
+        </div>
+
+    </div>
+    
+    <!--   Core   -->
+    <script src="{{ asset('/assets/js/plugins/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <!--   Optional JS   -->
+    <script src="{{ asset('/assets/js/plugins/chart.js/dist/Chart.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/plugins/chart.js/dist/Chart.extension.js') }}"></script>
+    <!--   Argon JS   -->
+    <script src="{{ asset('/assets/js/argon-dashboard.min.js?v=1.1.2') }}"></script>
+    <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+    <script>
+        window.TrackJS &&
+            TrackJS.install({
+                token: "ee6fab19c5a04ac1a32a645abde4613a",
+                application: "argon-dashboard-free"
+            });
+
+    </script>
+</body>
+
+</html>u    
