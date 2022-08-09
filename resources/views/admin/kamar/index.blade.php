37a463b06604bf6f8c8fb8323aef480028fadba5
raphaelimmanuel1711 <rph171104@gmail.com>
2022-08-09 22:44:51 +0700

    mockup

---
+++ resources/views/admin/kamar/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.main')
+
+@section('content')
+<h1 class="text-center">Kamar</h1>
+
+<a href="/tambahfasilitasumum" class="btn btn-success mt-5 mb-4">tambah</a>
+<table class="table table-light table-striped">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">No Kamar</th>
+            <th scope="col">Harga</th>
+            <th scope="col">Kapasitas</th>
+            <th scope="col">Tipe</th>
+            <th scope="col">Action</th>
+        </tr>
+    </thead>
+    @php
+        $no=1
+    @endphp
+    <tbody>
+        @foreach ($data as $row)            
+        <tr>
+            <th scope="row">{{ $loop->iteration  }}</th>
+            <td>{{ $row->kamar }}</td>
+            <td>{{ $row->nama }}</td>
+            <td style="display: flex;">
+                <form action="/deletedatafasilitasumum/{{$row->id}}" method="post" style="margin-right: 20px">
+                  @csrf
+                  <button type="submit" class="btn btn-danger">Hapus</button>
+                  @method('delete')
+                </form>
+                <a type="button" href="/tampilanfasilitasumum/{{$row->id}}" class="btn btn-warning">Edit</a>                                                         
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection
